0d2d6af8d01899714d058e011f2dbee6deafb2cc
Dadir-Dev <196910954+Dadir-Dev@users.noreply.github.com>
2026-01-15 22:23:13 +0300

    feat(products): add more products to database

--- data/products.js
+++ data/products.js
@@ -52,4 +52,523 @@ export const products = [
       "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80",
     category: "Electronics",
   },
+  {
+    id: 5,
+    name: "Nike Shoes",
+    price: 50,
+    originalPrice: null,
+    rating: {
+      stars: 4.5,
+      count: 8234,
+    },
+    image:
+      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Shoes",
+  },
+  {
+    id: 6,
+    name: "Two blank white mugs",
+    price: 23,
+    originalPrice: 30,
+    rating: {
+      stars: 4,
+      count: 478,
+    },
+    image:
+      "https://plus.unsplash.com/premium_photo-1719289799376-d3de0ca4ddbc?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Appliances",
+  },
+  {
+    id: 7,
+    name: "Android Smartwatch",
+    price: 120,
+    originalPrice: 200,
+    rating: {
+      stars: 4.5,
+      count: 4899,
+    },
+    image:
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?q=80&w=1099&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "accessories",
+  },
+  {
+    id: 8,
+    name: "Minimalist Leather Wallet",
+    price: 18.99,
+    originalPrice: 25.99,
+    rating: { stars: 4.5, count: 342 },
+    image:
+      "https://plus.unsplash.com/premium_photo-1681589453747-53fd893fa420?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Accessories",
+  },
+  {
+    id: 9,
+    name: "Stainless Steel Water Bottle",
+    price: 15.5,
+    originalPrice: null,
+    rating: { stars: 4, count: 811 },
+    image:
+      "https://images.unsplash.com/photo-1664714628878-9d2aa898b9e3?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Appliances",
+  },
+  {
+    id: 10,
+    name: "Gaming Mechanical Keyboard",
+    price: 74.99,
+    originalPrice: 99.99,
+    rating: { stars: 4.5, count: 1299 },
+    image:
+      "https://plus.unsplash.com/premium_photo-1664194583917-b0ba07c4ce2a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Electronics",
+  },
+  {
+    id: 11,
+    name: "Wireless Mouse",
+    price: 24.99,
+    originalPrice: 34.99,
+    rating: { stars: 4, count: 2201 },
+    image:
+      "https://images.unsplash.com/photo-1647755814392-fd071a3fcb4b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Electronics",
+  },
+  {
+    id: 12,
+    name: "Yoga Exercise Mat",
+    price: 29.99,
+    originalPrice: null,
+    rating: { stars: 4.5, count: 654 },
+    image:
+      "https://images.unsplash.com/photo-1746796751590-a8c0f15d4900?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Sports",
+  },
+  {
+    id: 13,
+    name: "Men's Casual Denim Jacket",
+    price: 59.99,
+    originalPrice: 79.99,
+    rating: { stars: 4, count: 187 },
+    image:
+      "https://images.unsplash.com/photo-1731341869905-5ef9bcd272f1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Clothing",
+  },
+  {
+    id: 14,
+    name: "Portable Bluetooth Speaker",
+    price: 39.99,
+    originalPrice: 59.99,
+    rating: { stars: 4.5, count: 2013 },
+    image:
+      "https://plus.unsplash.com/premium_photo-1728978926426-18fdb1d00e8a?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Electronics",
+  },
+  {
+    id: 15,
+    name: "Ceramic Dinner Plate Set (4 pcs)",
+    price: 45,
+    originalPrice: 60,
+    rating: { stars: 4, count: 98 },
+    image:
+      "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?auto=format&fit=crop&w=400&q=80",
+    category: "Appliances",
+  },
+  {
+    id: 16,
+    name: "Running Sneakers",
+    price: 69.99,
+    originalPrice: 89.99,
+    rating: { stars: 4.5, count: 3204 },
+    image:
+      "https://images.unsplash.com/photo-1549298916-b41d501d3772?auto=format&fit=crop&w=400&q=80",
+    category: "Shoes",
+  },
+  {
+    id: 17,
+    name: "Women's Sunglasses",
+    price: 22.99,
+    originalPrice: null,
+    rating: { stars: 4, count: 410 },
+    image:
+      "https://images.unsplash.com/photo-1511499767150-a48a237f0083?auto=format&fit=crop&w=400&q=80",
+    category: "Accessories",
+  },
+  {
+    id: 18,
+    name: "USB-C Fast Charging Cable",
+    price: 9.99,
+    originalPrice: 14.99,
+    rating: { stars: 4.5, count: 1890 },
+    image:
+      "https://images.unsplash.com/photo-1583394838336-acd977736f90?auto=format&fit=crop&w=400&q=80",
+    category: "Electronics",
+  },
+  {
+    id: 19,
+    name: "Men's Slim Fit Chinos",
+    price: 34.99,
+    originalPrice: 49.99,
+    rating: { stars: 4, count: 312 },
+    image:
+      "https://images.unsplash.com/photo-1521577352947-9bb58764b69a?auto=format&fit=crop&w=400&q=80",
+    category: "Clothing",
+  },
+  {
+    id: 20,
+    name: "Ergonomic Office Chair",
+    price: 189.99,
+    originalPrice: 249.99,
+    rating: { stars: 4.5, count: 874 },
+    image:
+      "https://images.unsplash.com/photo-1681418659069-eef28d44aeab?q=80&w=1161&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Furniture",
+  },
+  {
+    id: 21,
+    name: "Noise Isolating Earbuds",
+    price: 29.99,
+    originalPrice: null,
+    rating: { stars: 4, count: 2211 },
+    image:
+      "https://images.unsplash.com/photo-1590658268037-6bf12165a8df?auto=format&fit=crop&w=400&q=80",
+    category: "Electronics",
+  },
+  {
+    id: 22,
+    name: "Classic Baseball Cap",
+    price: 14.5,
+    originalPrice: null,
+    rating: { stars: 4.5, count: 645 },
+    image:
+      "https://images.unsplash.com/photo-1521369909029-2afed882baee?auto=format&fit=crop&w=400&q=80",
+    category: "Accessories",
+  },
+  {
+    id: 23,
+    name: "Electric Coffee Grinder",
+    price: 54.99,
+    originalPrice: 69.99,
+    rating: { stars: 4, count: 508 },
+    image:
+      "https://images.unsplash.com/photo-1603999684548-0002fd13a45d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8RWxlY3RyaWMlMjBDb2ZmZWUlMjBHcmluZGVyfGVufDB8MHwwfHx8MA%3D%3D",
+    category: "Appliances",
+  },
+  {
+    id: 24,
+    name: "Smart LED Desk Lamp",
+    price: 42.99,
+    originalPrice: 59.99,
+    rating: { stars: 4.5, count: 933 },
+    image:
+      "https://images.unsplash.com/photo-1519710164239-da123dc03ef4?auto=format&fit=crop&w=400&q=80",
+    category: "Electronics",
+  },
+  {
+    id: 25,
+    name: "Women's Running Shoes",
+    price: 79.99,
+    originalPrice: 99.99,
+    rating: { stars: 4.5, count: 2104 },
+    image:
+      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=400&q=80",
+    category: "Shoes",
+  },
+  {
+    id: 26,
+    name: "Hardcover Notebook Journal",
+    price: 12.99,
+    originalPrice: null,
+    rating: { stars: 4, count: 158 },
+    image:
+      "https://images.unsplash.com/photo-1519682337058-a94d519337bc?auto=format&fit=crop&w=400&q=80",
+    category: "Stationery",
+  },
+  {
+    id: 27,
+    name: "Stainless Steel Cookware Pan",
+    price: 64.99,
+    originalPrice: 89.99,
+    rating: { stars: 4.5, count: 731 },
+    image:
+      "https://images.unsplash.com/photo-1745710489337-47d1c0aa6931?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Appliances",
+  },
+  {
+    id: 28,
+    name: "Fitness Resistance Bands Set",
+    price: 21.99,
+    originalPrice: 29.99,
+    rating: { stars: 4.5, count: 1402 },
+    image:
+      "https://images.unsplash.com/photo-1599058917212-d750089bc07e?auto=format&fit=crop&w=400&q=80",
+    category: "Sports",
+  },
+  {
+    id: 29,
+    name: "Digital Alarm Clock",
+    price: 18.99,
+    originalPrice: null,
+    rating: { stars: 4, count: 602 },
+    image:
+      "https://images.unsplash.com/photo-1638245578732-238324d93487?q=80&w=1072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Electronics",
+  },
+  {
+    id: 30,
+    name: "Casual Canvas Backpack",
+    price: 49.99,
+    originalPrice: 69.99,
+    rating: { stars: 4.5, count: 982 },
+    image:
+      "https://images.unsplash.com/photo-1509762774605-f07235a08f1f?auto=format&fit=crop&w=400&q=80",
+    category: "Accessories",
+  },
+  {
+    id: 31,
+    name: "Men's Analog Wrist Watch",
+    price: 129.99,
+    originalPrice: 179.99,
+    rating: { stars: 4.5, count: 1187 },
+    image:
+      "https://images.unsplash.com/photo-1524805444758-089113d48a6d?auto=format&fit=crop&w=400&q=80",
+    category: "Accessories",
+  },
+  {
+    id: 32,
+    name: "Non-Stick Baking Tray",
+    price: 16.99,
+    originalPrice: null,
+    rating: { stars: 4, count: 244 },
+    image:
+      "https://images.unsplash.com/photo-1608198093002-ad4e005484ec?auto=format&fit=crop&w=400&q=80",
+    category: "Appliances",
+  },
+  {
+    id: 33,
+    name: "Bluetooth Car Adapter",
+    price: 27.99,
+    originalPrice: 39.99,
+    rating: { stars: 4.5, count: 1650 },
+    image:
+      "https://images.unsplash.com/photo-1725608940791-99897fccdaee?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Electronics",
+  },
+  {
+    id: 34,
+    name: "Women's Knit Sweater",
+    price: 44.99,
+    originalPrice: 59.99,
+    rating: { stars: 4, count: 398 },
+    image:
+      "https://images.unsplash.com/photo-1512436991641-6745cdb1723f?auto=format&fit=crop&w=400&q=80",
+    category: "Clothing",
+  },
+  {
+    id: 35,
+    name: "Adjustable Dumbbells Set",
+    price: 149.99,
+    originalPrice: 199.99,
+    rating: { stars: 4.5, count: 902 },
+    image:
+      "https://images.unsplash.com/photo-1583454110551-21f2fa2afe61?auto=format&fit=crop&w=400&q=80",
+    category: "Sports",
+  },
+  {
+    id: 36,
+    name: "Cordless Electric Kettle",
+    price: 39.99,
+    originalPrice: 55.99,
+    rating: { stars: 4, count: 711 },
+    image:
+      "https://images.unsplash.com/photo-1545259741-2ea3ebf61fa3?auto=format&fit=crop&w=400&q=80",
+    category: "Appliances",
+  },
+  {
+    id: 37,
+    name: "Laptop Sleeve Case 15-inch",
+    price: 22.99,
+    originalPrice: null,
+    rating: { stars: 4.5, count: 1330 },
+    image:
+      "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?auto=format&fit=crop&w=400&q=80",
+    category: "Accessories",
+  },
+  {
+    id: 38,
+    name: "Wireless Charging Pad",
+    price: 19.99,
+    originalPrice: 29.99,
+    rating: { stars: 4, count: 2044 },
+    image:
+      "https://images.unsplash.com/photo-1586953208448-b95a79798f07?auto=format&fit=crop&w=400&q=80",
+    category: "Electronics",
+  },
+  {
+    id: 39,
+    name: "Ceramic Vase Decor",
+    price: 28.5,
+    originalPrice: null,
+    rating: { stars: 4, count: 176 },
+    image:
+      "https://images.unsplash.com/photo-1501045661006-fcebe0257c3f?auto=format&fit=crop&w=400&q=80",
+    category: "Home",
+  },
+  {
+    id: 40,
+    name: "Memory Foam Pillow",
+    price: 36.99,
+    originalPrice: 49.99,
+    rating: { stars: 4.5, count: 2890 },
+    image:
+      "https://images.unsplash.com/photo-1673201102066-b0599d45002b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Home",
+  },
+  {
+    id: 41,
+    name: "Smartphone Tripod Stand",
+    price: 17.99,
+    originalPrice: null,
+    rating: { stars: 4, count: 901 },
+    image:
+      "https://images.unsplash.com/photo-1706372124814-417e2f0c3fe0?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Electronics",
+  },
+  {
+    id: 42,
+    name: "Leather Crossbody Bag",
+    price: 79.99,
+    originalPrice: 109.99,
+    rating: { stars: 4.5, count: 544 },
+    image:
+      "https://images.unsplash.com/photo-1584917865442-de89df76afd3?auto=format&fit=crop&w=400&q=80",
+    category: "Accessories",
+  },
+  {
+    id: 43,
+    name: "Electric Toothbrush",
+    price: 59.99,
+    originalPrice: 89.99,
+    rating: { stars: 4.5, count: 3201 },
+    image:
+      "https://images.unsplash.com/photo-1612181819081-950d35f4d826?q=80&w=1043&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Health",
+  },
+  {
+    id: 44,
+    name: "Cotton Bath Towel Set",
+    price: 34.99,
+    originalPrice: null,
+    rating: { stars: 4, count: 417 },
+    image:
+      "https://images.unsplash.com/photo-1600369672770-985fd30004eb?auto=format&fit=crop&w=400&q=80",
+    category: "Home",
+  },
+  {
+    id: 45,
+    name: "Portable Handheld Fan",
+    price: 13.99,
+    originalPrice: 19.99,
+    rating: { stars: 4, count: 1382 },
+    image:
+      "https://plus.unsplash.com/premium_photo-1752929587329-4cb507a964f8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Electronics",
+  },
+  {
+    id: 46,
+    name: "Adjustable Phone Holder",
+    price: 11.99,
+    originalPrice: null,
+    rating: { stars: 4.5, count: 2440 },
+    image:
+      "https://images.unsplash.com/photo-1659540820064-754508057a98?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Accessories",
+  },
+  {
+    id: 47,
+    name: "Stainless Steel Cutlery Set",
+    price: 49.99,
+    originalPrice: 69.99,
+    rating: { stars: 4, count: 283 },
+    image:
+      "https://images.unsplash.com/photo-1706028212203-a2be8979f964?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Appliances",
+  },
+  {
+    id: 48,
+    name: "Women's Summer Dress",
+    price: 39.99,
+    originalPrice: 54.99,
+    rating: { stars: 4.5, count: 1201 },
+    image:
+      "https://images.unsplash.com/photo-1490481651871-ab68de25d43d?auto=format&fit=crop&w=400&q=80",
+    category: "Clothing",
+  },
+  {
+    id: 49,
+    name: "Foldable Travel Duffel Bag",
+    price: 27.99,
+    originalPrice: null,
+    rating: { stars: 4, count: 611 },
+    image:
+      "https://images.unsplash.com/photo-1522199755839-a2bacb67c546?auto=format&fit=crop&w=400&q=80",
+    category: "Accessories",
+  },
+  {
+    id: 50,
+    name: "LED String Lights",
+    price: 19.99,
+    originalPrice: 29.99,
+    rating: { stars: 4.5, count: 3442 },
+    image:
+      "https://images.unsplash.com/photo-1504196606672-aef5c9cefc92?auto=format&fit=crop&w=400&q=80",
+    category: "Home",
+  },
+  {
+    id: 51,
+    name: "Digital Kitchen Scale",
+    price: 24.99,
+    originalPrice: null,
+    rating: { stars: 4, count: 835 },
+    image:
+      "https://images.unsplash.com/photo-1586953208448-b95a79798f07?auto=format&fit=crop&w=400&q=80",
+    category: "Appliances",
+  },
+  {
+    id: 52,
+    name: "Men's Casual Sneakers",
+    price: 64.99,
+    originalPrice: 84.99,
+    rating: { stars: 4.5, count: 1983 },
+    image:
+      "https://images.unsplash.com/photo-1562538757-ed20f0ee395b?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Shoes",
+  },
+  {
+    id: 53,
+    name: "Smart Wi-Fi Plug",
+    price: 17.99,
+    originalPrice: 24.99,
+    rating: { stars: 4, count: 1449 },
+    image:
+      "https://images.unsplash.com/photo-1586953208448-b95a79798f07?auto=format&fit=crop&w=400&q=80",
+    category: "Electronics",
+  },
+  {
+    id: 54,
+    name: "Insulated Lunch Box",
+    price: 21.99,
+    originalPrice: null,
+    rating: { stars: 4, count: 392 },
+    image:
+      "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?auto=format&fit=crop&w=400&q=80",
+    category: "Accessories",
+  },
+  {
+    id: 57,
+    name: "Portable Power Bank 20000mAh",
+    price: 49.99,
+    originalPrice: 69.99,
+    rating: { stars: 4.5, count: 5421 },
+    image:
+      "https://images.unsplash.com/photo-1609091839311-d5365f9ff1c5?auto=format&fit=crop&w=400&q=80",
+    category: "Electronics",
+  },
 ];
